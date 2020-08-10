de69dc96271cfbb0b8e30ad49f08ff30bd389a0e
Arthur Koehl <avkoehl@ucdavis.edu>
2020-08-10 11:04:32 -0700

    test run on full set with timer

--- corpus.py
+++ corpus.py
@@ -1,3 +1,4 @@
+import time
 from pathlib import Path
 
 from joblib import Parallel, delayed
@@ -8,11 +9,14 @@ Path('../data/extracted_text/').mkdir(parents=True, exist_ok=True)
 p = Path("../data/")
 files = list(p.glob('**/*.xml'))
 
-results = [px.parse_xml(fname) for fname in files[0:20]]
 
-#results = Parallel(n_jobs=4)(delayed(parse_xml)(f) for f in files[0:2000])
-#for i in range(0, len(results)):
-#    with open("../data/extracted_text/" + files[i].stem + ".txt", 
-#            encoding="utf-8",
-#            mode="w") as ofile:
-#        ofile.write(results[i][1])
+print("running on all files 4 cores")
+start = time.time()
+results = Parallel(n_jobs=4)(delayed(px.parse_xml)(f) for f in files)
+for i in range(0, len(results)):
+    with open("../data/extracted_text/" + files[i].stem + ".txt", 
+            encoding="utf-8",
+            mode="w") as ofile:
+        ofile.write(results[i][1])
+
+print("elapsed time: ", time.time() - start, " seconds")
