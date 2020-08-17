b2c9cb284e54c93018653d769f25bbcaeaaa3ba5
avkoehl <avkoehl@ucdavis.edu>
2020-08-17 14:23:27 -0700

    added size estimate to determine if need to use gridfs

--- corpus.py
+++ corpus.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import sys
 
 from joblib import Parallel, delayed
+import json
 
 import python.parse_xml as px
 import python.mongo as m
@@ -9,23 +11,32 @@ print("connecting to mongo database")
 mydb = m.open_db_connection("mongo-credentials.json")
 
 print("parsing xml")
-files = list(Path("../data/").glob('**/*.xml'))[0:100]
-results = Parallel(n_jobs=4)(delayed(px.parse_xml)(f) for f in files)
+files = list(Path("../data/").glob('**/*.xml'))
+results = Parallel(n_jobs=20)(delayed(px.parse_xml)(f) for f in files[1:10000])
 
 print("inserting into database")
 metadata_list = []
 content_list = []
 truncated_content_list = []
+large_content_list = []
 for i,result in enumerate(results):
     result[0]["_id"] = i
     content = {"_id": i,
                "raw": result[1]}
     truncated = {"_id": i,
                  "raw": result[1][0:500]}
+
+    #print(len(json.dumps(content)) / 1000000)
+    size_estimate = sys.getsizeof(json.dumps(content).encode('utf8')) / 1000000
+    if (size_estimate > 4):
+        print(i, size_estimate)
+
+
     metadata_list.append(result[0])
     content_list.append(content)
     truncated_content_list.append(truncated)
 
 
-m.insert_metadata(mydb, metadata_list)
-m.insert_content(mydb, content_list, truncated_content_list)
+#m.insert_metadata(mydb, metadata_list)
+#m.insert_content(mydb, content_list, truncated_content_list)
+#m.insert_large_content(mydb, large_content_list)
--- python/mongo.py
+++ python/mongo.py
@@ -12,7 +12,7 @@ def load_credentials(fname):
             "user": data["user"],
             "pwd": data["pwd"] })
 
-def open_db_connection(fname, overwrite = False):
+def open_db_connection(fname, overwrite = True):
     credentials = load_credentials(fname)
     myclient = pymongo.MongoClient(host = credentials["host"],
         port= credentials["port"])
@@ -35,5 +35,11 @@ def insert_metadata(mydb, metadata_list):
     mydb["docs.metadata"].insert_many(metadata_list)
 
 def insert_content(mydb, content_list, truncated_content_list):
+    large_content_list = []
+    # iterate through content list, moving documents that are too large into another list
+
+    # insert the large documents using grid fs
+
+    # finally insert the two lists (truncated and remaining content_list items
     mydb["docs"].insert_many(content_list)
     mydb["docs.truncated"].insert_many(truncated_content_list)
