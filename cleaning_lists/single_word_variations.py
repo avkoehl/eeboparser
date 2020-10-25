f6a48932ff8c19d1079a61e9d83db7aae346bdca
Arthur Koehl <avkoehl@ucdavis.edu>
2020-10-25 00:02:58 -0700

    getting there

---
+++ cleaning_lists/single_word_variations.py
@@ -0,0 +1,372 @@
+# individual word variations
+# based on a non-experts reading of unique locations
+# in the locations column sorted alphabetically
+# variations are due to spelling, language and transcription errors
+single_word_variations = {
+#
+# BASEL (Basel, Switzerland)
+"basil": "basel",
+"basill": "basel",
+"basyl": "basel",
+#
+# DOWAY (Douai, France)
+"douay": "doway",
+"douai": "doway",
+"douay": "doway", 
+"doüay": "doway", 
+"dovvay": "doway",
+"dovvay": "doway", 
+"dowa": "doway",
+"dowaie": "doway",
+#
+# DENSHIRE (unknown)
+"denshyre": "denshire",
+#
+# DUBLIN (Dublin, Ireland)
+"dubdin": "dublin",
+"dubline": "dublin",
+"dublinii": "dublin",
+"dvblin": "dublin",
+#
+# FRANKFURT (Frankfurt, Germany)
+"franckford": "frankfurt",
+"franckfort": "frankfurt",
+"francofourd": "frankfrut",
+#
+# EDINBURGH (Edinburgh, Scotland)
+"eclinburgh": "edinburgh",
+"edenborough": "edinburgh",
+"edenbrough": "edinburgh",
+"edenburg": "edinburgh",
+"edenburgh": "edinburgh",
+"edenburgi": "edinburgh",
+"ediburgh": "edinburgh",
+"edimbourg": "edinburgh",
+"edinb": "edinburgh",
+"edinbourg": "edinburgh",
+"edinbrugh": "edinburgh",
+"edinburg":  "edinburgh",
+"edinburghi": "edinburgh",
+"edinburgi": "edinburgh",
+"edinburh": "edinburgh",
+"edinbvrgh": "edinburgh",
+"edingborough": "edinburgh",
+"edingburgh": "edinburgh",
+"edinnburgi": "edinburgh",
+"edynburgh": "edinburgh",
+#
+# MARLBOROUGH (Marlborough, England)
+"malborow": "marlborough",
+"marborow": "marlborough",
+"marlborow": "marlborough",
+#
+# DELFT (Delf, Netherlands)
+"delf": "delft",
+"delff": "delft",
+"delph": "delft",
+#
+# CANTERBURY (Canterbury, England)
+"cantabrigiae": "canterbury",
+"cantabrigiæ": "canterbury",
+"cantebrig": "canterbury",
+"cantorbury": "canterbury", 
+"cantebrigiæ": "canterbury",
+"canterbury": "canterbury",
+"cantorbury": "canterbury",
+#
+# COLLEN (unknown)
+"colloniæ": "collen",
+#
+# CORK (Cork, Ireland)
+"corck": "cork",
+"corcke": "cork",
+"cork": "cork",
+#
+# MALMO (Malmo, Sweden)
+"malmoo": "malmo",
+"malmö": "malmo",
+#
+# PARIS (Paris, France)
+"parisiis": "paris",
+"parisius": "paris",
+"parise": "paris",
+"parys": "paris",
+#
+# WESTMINSTER (Westminster, England)
+"westmystre": "westminster",
+"westmonstre": "westminster",
+"westmynster": "westminster",
+"westmynstre": "westminster",
+"westmyster": "westminster",
+"westmonasterii": "westminster",
+"westmestre": "westminster",
+"westmynstre": "westminster",
+"westmoster": "westminster",
+"westemynster": "westminster",
+"westmester": "westminster",
+"westmestre": "westminster",
+"westminister": "westminster",
+#
+# WITTENBERG (Wittenberg, Germany)
+"wittenberge": "witteberg",
+#
+# HEMEL HEMPSTEAD (Hemel Hempstead, England)
+"hemstead": "hemel hempstead"
+#
+# HEIDELBERG (Heidelberg, Germany)
+"heidelbergh": "heidelberg",
+#
+# HAMBURG (Hamburg, Germany)
+"hamborough": "hamburg",
+"hamboroughe": "hambrug",
+"hamburgh": "hambrug",
+#
+# DURHAM (Durham, England)
+# gatesehead maps to durham because of appearance of gateside ie gateshead durham
+"gateshead": "durham",
+"gateside": "durham",
+#
+# GHENT (Ghent, Belgium)
+"gente": "ghent",
+"gant": "ghent",
+#
+# MWYTHIG (unknown)
+"mwŷthig": "mwythig",
+#
+# BRISTOL (Bristol, England)
+"bristoll": "bristol",
+#
+# BRUSSELS (Brussels, Belgium)
+"bruxelles": "brussels"
+"bruxells": "brussels",
+#
+# GLASGOW
+"glasgovv": "glasgow",
+"glasguæ": "glasgow",
+#
+# GENEVA
+"geneua"
+"genevah"
+#
+# CAMBRDIGE
+cambridg
+#
+# GREENWICH
+grenewych
+#
+#VENICE
+venetia
+ventiis
+venus
+venez
+#
+# TOULOUSE
+tolose
+#
+# NETHERLANDS
+lowcountryes
+#
+# LOUVAIN
+louain
+louaine
+louan
+louanii
+louayne
+lovain
+lovanii
+lovanni
+#
+# OXFORD
+oxoniæ
+oxonii
+oxen
+oxforde
+oxenford
+#
+# SOUTHWARK
+southwarke
+sowthwarke
+sothewarke
+#
+# LONDON
+londra
+oonden
+nod-nol
+lunnduin
+lundain
+lundon
+loydon
+loudon
+londonn
+londoon
+londonian
+londou
+londra
+londre
+londrun
+lonini
+lonkon
+lonlon
+lonndini
+lonndo
+lonndon
+lonnon
+lonodnsic
+lonon
+londnon
+londom
+lo
+loddon
+lodnon
+lodon
+lond
+londen
+londgn
+londin
+londrez
+londonijs
+londres
+lond
+llùndain
+londdn
+londinesis
+llundain
+londini
+lbndon
+#
+# HARLEM
+haarlem
+#
+# HAGUE
+hagæ
+hage
+hagh
+haghe
+#
+# AMSTERDAM
+amstelrodam
+ampsterdam
+amstelodami
+amselredam
+amstelredam
+amsterledam
+amsterodam
+amstredam
+antwerpen
+#
+# EUROPE
+europ
+#
+# ANTWERP
+andwarpe
+andewerpe
+andwerpe
+antuarpe
+antuerpiae
+antuerpiæ
+antverpiae
+antberpiæ
+antvverp
+antvverpe
+antwarp
+antwerp
+antwerpe
+antwerpen
+antwerpiæ
+antwrop
+anvverpe
+anwerp
+anwerpe
+#
+# IPSWICH
+ippeswich
+ippeswych
+ippiswich
+ippiswiche
+ippyswiche
+ippyswyche
+#
+# LEIDEN
+leyden
+#
+# LIEGE
+liège
+#
+# WITTENBORG
+wittonburge
+wyttonburge
+#
+# BLACKFRIERS
+blackefriers
+#
+# ROTTERDAM
+roterdame
+#
+# ABERDEEN
+aberden
+aberdene
+aberdoniæ
+aberdoniis
+abredeis
+#
+# MIDDLEBURG
+middelborg
+middelborough
+middelburg
+middelburgh
+middleborrowgh
+middleborrow
+middleborugh
+middlebourgh
+middleburgh
+middlebrugh
+midleburgh
+#
+# ROUEN
+roan
+rouan
+roüen
+rowen
+#
+# RHEIMS
+rheins
+rhemes
+# 
+# SAINT ANDREWS
+sanctandrois
+#
+# SAVOY
+saouy
+savoye
+#
+#STRASBOURG
+strasborowe
+straszburg
+#
+#STRIVELING
+striuling
+#
+# YORK
+yorke
+#
+# WORCESTER
+worceter
+#
+# ZURICH
+zijrik
+zurych
+zürich
+ziiryk
+zurik
+#
+# WATERFORD
+vvaterford
+warerford
+#
+# WESEL
+wesill
+#
+# UTRECTH
+vtrech
+vtrecth
+vtricht
