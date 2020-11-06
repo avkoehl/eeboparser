4955ad893fcd0afbbe51e0f44606c362202dea2c
Arthur Koehl <avkoehl@ucdavis.edu>
2020-11-05 23:15:47 -0800

    removing from main for easier debugging

--- 1_parse_xml.py
+++ 1_parse_xml.py
@@ -20,31 +20,31 @@ from eeboparser import parse_xml
 from eeboparser import utils
 
 
-def main(db=False, input_dir = "../data/", ncores=4):
+db = False
+input_dir = "../data/"
+ncores = 4
 
-    print("reading in xml documents")
-    files = list(Path(input_dir).glob('**/*.xml'))
-    xml = [utils.get_xml_content(i, fname) for i, fname in enumerate(files[0:500])]
-    
-    print("parsing xml")
-    results = Parallel(n_jobs=ncores)(delayed(parse_xml.parse_xml)(x["_id"], 
-        x["xml"]) for x in xml)
-    meta, texts,truncated = zip(*results)
-    
-    # clean meta
-    
-    if db:
-        print("connecting to mongo database")
-        mydb = mongo.open_db_connection("mongo-credentials.json")
-        for col in ["docs.xml", "docs.xml.chunks", "docs.xml.files", 
-                "docs.meta", "docs.text", "docs.truncated"]:
-            mydb[col].drop()
+print("reading in xml documents")
+files = list(Path(input_dir).glob('**/*.xml'))
+xml = [utils.get_xml_content(i, fname) for i, fname in enumerate(files[0:500])]
 
-        print("write to database")
-        mongo.insert_xml_gridfs(mydb, "docs.xml", xml)
-        r = mydb["docs.meta"].insert_many(meta)
-        r = mydb["docs.truncated"].insert_many(truncated)
-        r = mydb["docs.text"].insert_many(texts)
+print("parsing xml")
+results = Parallel(n_jobs=ncores)(delayed(parse_xml.parse_xml)(x["_id"], 
+    x["xml"]) for x in xml)
+meta, texts,truncated = zip(*results)
 
-if __name__ == "__main__":
-    main()
+# clean meta
+
+if db:
+    print("connecting to mongo database")
+    mydb = mongo.open_db_connection("mongo-credentials.json")
+    for col in ["docs.xml", "docs.xml.chunks", "docs.xml.files", 
+            "docs.meta", "docs.text", "docs.truncated"]:
+        mydb[col].drop()
+
+    print("write to database")
+    mongo.insert_xml_gridfs(mydb, "docs.xml", xml)
+    r = mydb["docs.meta"].insert_many(meta)
+    r = mydb["docs.truncated"].insert_many(truncated)
+    r = mydb["docs.text"].insert_many(texts)
+
