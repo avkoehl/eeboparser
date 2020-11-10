322d73232acde898147f4bf09991865f1a5e53fd
Arthur Koehl <avkoehl@ucdavis.edu>
2020-11-09 16:10:31 -0800

    read in columns as list when appropriate

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from eeboparser import clean_meta
 
 meta = pd.read_csv("./data/meta.csv", dtype={'Locations': str})
 meta['Keywords'] = meta['Keywords'].apply(lambda x: ast.literal_eval(x))
+meta['Author'] = meta['Author'].apply(lambda x: ast.literal_eval(x))
+meta['Language'] = meta['Language'].apply(lambda x: ast.literal_eval(x))
 
     # TODO verify results with value_counts and nunique, and comparisons
     # such as print all the original values that mapped to london...
